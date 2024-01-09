2ca08f6ad1ff3e1799f4d022171adc37e08094bf
vienna-hev <stardog727@gmail.com>
2024-01-09 14:42:55 -0500

    blah
    f

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp.java
@@ -1,11 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
+
 public class teleOp extends LinearOpMode {
 
     public DcMotor frontRight;
@@ -16,8 +19,7 @@ public class teleOp extends LinearOpMode {
     public Servo clawGrabber;
     public DcMotor clawWrist;
     public DcMotor clawArm;
-
-   // public Servo airplaneLauncher;
+    public Servo airplaneLauncher;
 
     @Override
     public void runOpMode() {
@@ -30,7 +32,6 @@ public class teleOp extends LinearOpMode {
 
         backRight = hardwareMap.get(DcMotor.class, "backRight");
 
-
         clawGrabber = hardwareMap.get(Servo.class, "clawGrabber");
 
         clawWrist = hardwareMap.get(DcMotor.class, "clawWrist");
@@ -39,124 +40,117 @@ public class teleOp extends LinearOpMode {
 
         clawArm = hardwareMap.get(DcMotor.class, "clawArm");
 
-        // airplaneLauncher?!!!
-       // airplaneLauncher = hardwareMap.get(Servo.class, "airplaneLauncher");
-        
+        airplaneLauncher = hardwareMap.get(Servo.class, "airplaneLauncher");
+
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        double drivespeed = 0.8;
+
 
         waitForStart();
 
         while (opModeIsActive()) {
-
-            double speed = -gamepad1.left_stick_y;
-            double strafe = -gamepad1.left_stick_x;
-            double turn = -gamepad1.right_stick_x;
-
-            frontLeft.setPower    (speed + turn - strafe);
-            frontRight.setPower   (speed - turn + strafe);
-            backLeft.setPower     (speed + turn + strafe);
-            backRight.setPower    (speed - turn - strafe);
-
-            if (gamepad1.right_bumper) {
-                frontLeft.setPower(-1);
-                frontRight.setPower(-1);
-                backLeft.setPower(-1);
-                backRight.setPower(1);
-            }
-            if (!gamepad1.right_bumper) {
-                frontLeft.setPower(0);
-                frontRight.setPower(0);
-                backLeft.setPower(0);
-                backRight.setPower(0);
-            }
-            if (gamepad1.left_bumper) {
-                frontLeft.setPower(1);
-                frontRight.setPower(1);
-                backLeft.setPower(1);
-                backRight.setPower(-1);
-                            }
-            if (!gamepad1.left_bumper) {
-                frontLeft.setPower(0);
-                frontRight.setPower(0);
-                backLeft.setPower(0);
-                backRight.setPower(0);
-            }
-
-//            if (gamepad1.left_stick_y == 1) {
-//                frontLeft.setPower(0.5);
-//                frontRight.setPower(-0.5);
-//                backLeft.setPower(0.5);
-//                backRight.setPower(0.5);
+//            if (gamepad1.right_bumper) {
+//                frontLeft.setPower(-drivespeed);
+//                frontRight.setPower(drivespeed);
+//                backLeft.setPower(-drivespeed);
+//                backRight.setPower(drivespeed);
 //            }
-//            if (gamepad1.left_stick_y == -1) {
-//                frontLeft.setPower(-1);
-//                frontRight.setPower(1);
-//                backLeft.setPower(-1);
-//                backRight.setPower(-1);
+//            else if (gamepad1.left_bumper) {
+//                frontLeft.setPower(drivespeed);
+//                frontRight.setPower(-drivespeed);
+//                backLeft.setPower(drivespeed);
+//                backRight.setPower(-drivespeed);
 //            }
-//            if (gamepad1.left_stick_y == 0) {
+//
+//            else if (gamepad1.left_stick_y > 0.9) {
+//                frontLeft.setPower(drivespeed);
+//                frontRight.setPower(drivespeed);
+//                backLeft.setPower(drivespeed);
+//                backRight.setPower(drivespeed);
+//            }
+//            else if (gamepad1.left_stick_y < -0.9) {
+//                frontLeft.setPower(-drivespeed);
+//                frontRight.setPower(-drivespeed);
+//                backLeft.setPower(-drivespeed);
+//                backRight.setPower(-drivespeed);
+//            }
+//
+//            else if (gamepad1.left_stick_x > 0.9) {
+//                frontLeft.setPower(-drivespeed);
+//                frontRight.setPower(drivespeed);
+//                backLeft.setPower(drivespeed);
+//                backRight.setPower(-drivespeed);
+//            }
+//            else if (gamepad1.left_stick_x < -0.9) {
+//                frontLeft.setPower(drivespeed);
+//                frontRight.setPower(-drivespeed);
+//                backLeft.setPower(-drivespeed);
+//                backRight.setPower(drivespeed);
+//            }
+//            else {
 //                frontLeft.setPower(0);
 //                frontRight.setPower(0);
 //                backLeft.setPower(0);
 //                backRight.setPower(0);
 //            }
-//            if (gamepad1.left_stick_x == 1) {
-//                frontLeft.setPower(-1);
-//                frontRight.setPower(-1);
-//                backLeft.setPower(1);
-//                backRight.setPower(-1);
-//            }
-//            if (gamepad1.left_stick_x == -1) {
-//                frontLeft.setPower(1);
-//                frontRight.setPower(1);
-//                backLeft.setPower(-1);
-//                backRight.setPower(1);
-//            }
-//            if (gamepad1.left_stick_x == 0) {
-//                frontLeft.setPower(0);
-//                frontRight.setPower(0);
-//                backLeft.setPower(0);
-//                backRight.setPower(0);
 
+            double speed = gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            double strafe = gamepad1.left_stick_x;
+            frontLeft.setPower(-speed + turn + strafe);
+            frontRight.setPower(-speed - turn - strafe);
+            backLeft.setPower(-speed + turn - strafe);
+            backRight.setPower(-speed - turn + strafe);
+
+            //hookLifter
             if (gamepad1.a) {
-                hookLifter.setPower(2);
+                hookLifter.setPower(1);
+            } else if (gamepad1.b) {
+                hookLifter.setPower(-1);
             } else {
                 hookLifter.setPower(0);
-            }
-            if (gamepad1.b) {
-                hookLifter.setPower(-2);
-            } else {
-                hookLifter.setPower(0);
-            }
-            if (gamepad2.dpad_up) {
-                clawArm.setPower(2);
-            }
-            if (gamepad2.dpad_down) {
-                clawArm.setPower(-2);
             }
 
-            if (gamepad2.left_stick_y == 1) {
-                clawWrist.setPower(2);
-            } else if (gamepad2.left_stick_y == -1) {
-                clawWrist.setPower(-2);
+            //clawArm
+            if (gamepad2.dpad_up) {
+                clawArm.setPower(.5);
+            } else if (gamepad2.dpad_down) {
+                clawArm.setPower(-.5);
+            } else {
+                clawArm.setPower(0);
+            }
+
+            //clawWrist
+            if (gamepad2.left_stick_y > .9) {
+                clawWrist.setPower(.5);
+            } else if (gamepad2.left_stick_y < -0.9) {
+                clawWrist.setPower(-.5);
             } else {
                 clawWrist.setPower(0);
             }
 
-            // set position? .25?
-            if (gamepad2.right_stick_x == 1) {
-                clawGrabber.setPosition(1);
-            }
-            if (gamepad2.right_stick_x == 0) {
+            //clawGrabber
+            if (gamepad2.right_stick_x > 0.9) {
+                clawGrabber.setPosition(.25);
+            } else if (gamepad2.right_stick_x < -0.9) {
+                clawGrabber.setPosition(-.25);
+            } else {
                 clawGrabber.setPosition(0);
             }
-            if (gamepad2.right_stick_x == -1) {
-                clawGrabber.setPosition(-1);
-            }
-          //  if (gamepad2.y) {
-            //    airplaneLauncher.setPosition(-1);
-            }
-         //   if (gamepad2.x) {
-               //  airplaneLauncher.setPosition(0);
+
+            //airplaneLauncher
+            if (gamepad2.x) {
+                airplaneLauncher.setPosition(1);
+            } else {
+                airplaneLauncher.setPosition(0);
             }
         }
+    }
 }
